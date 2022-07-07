55232be9f9ee36e4d37ce86dd4b87c6eb3656650
J Rockwell Seebach <jrs@ironpinephoto.com>
2022-07-06 22:22:28 -0700

    Add project files.

---
+++ CandU219/MathOPs.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CandU219
+{
+    class MathOPs
+    {
+
+        public int Num1 = 7;
+
+        public int result;
+        public static void methodONE(int Num1, int Num2)
+        {
+            int X = Num1;
+            int Y = Num2;
+            int result = X + Y;
+
+            Console.WriteLine("Seven plus the number(s) you entered = " + result);
+        }
+
+
+        public static void  methodONE(int Num1, int Num2, params int[] restOfNumbers)
+
+        {
+            int X = Num1;
+            int Y = Num2;
+            int result = X + Y;
+            if (restOfNumbers != null)
+            {
+                foreach (int i in restOfNumbers)
+                {
+                    result += i;
+                }
+
+                Console.WriteLine("Seven plus the number(s) you entered = " + result);
+            }
+        }
+
+        public static void methodONE(int Num1, int Num2, IEnumerable<int> restOfNumbers)
+
+        {
+            int X = Num1;
+            int Y = Num2;
+            int result = X + Y;
+            if (restOfNumbers != null)
+            {
+                foreach (int i in restOfNumbers)
+                {
+                    result += i;
+                }
+
+                Console.WriteLine("Seven plus the number(s) you entered = " + result);
+            }
+        }
+    }
+}
---
+++ CandU219/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CandU219
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            int Num1 = 7;
+
+            List<int> restOfNumbers = new List<int>();
+
+            bool loopOne = true;
+
+
+                Console.WriteLine("Please enter a number");
+                int Num2 = Convert.ToInt16(Console.ReadLine());
+
+
+
+            while (loopOne == true)
+            {
+                Console.WriteLine("If you would like to enter another number please do so now. \n" +
+                    "If not, please press 'Enter'");
+
+                string nextNum =Console.ReadLine();
+
+                if (string.IsNullOrEmpty(nextNum))
+                    {
+                    MathOPs.methodONE(Num1, Num2, restOfNumbers);
+                    Console.ReadLine();
+                    loopOne = false;
+                    break;
+                }
+                else
+                {
+                    int newInput = Convert.ToInt16(nextNum);
+                    restOfNumbers.Add(newInput);
+                }
+
+            }
+        }
+    }
+}
